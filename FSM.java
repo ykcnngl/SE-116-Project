ddc59bdf3dd3092bf4d46818b1e6e7c95ba11efa
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2025-05-02 01:24:16 +0300

    Update FSM.java

--- FSM.java
+++ FSM.java
@@ -1,1 +1,24 @@
-FSM CLASS
+package fsm.core;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+public class FSM implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private final List<String> symbols = new ArrayList<>();
+    private final List<String> states = new ArrayList<>();
+    private final List<String> finalStates = new ArrayList<>();
+    private final List<Transition> transitions = new ArrayList<>();
+    private String initialState = null;
+
+    public FSM() {}
+
+    public void clear() {
+        symbols.clear();
+        states.clear();
+        finalStates.clear();
+        transitions.clear();
+        initialState = null;
+    }
+}
